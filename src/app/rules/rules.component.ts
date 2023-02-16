92a31b3eb410a52d4af1bb7273c73649143252c9
danieleavolio <daniele.avolio14@gmail.com>
2023-02-16 23:23:47 +0100

    modified css and fixed things (prego bro)

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -6,6 +6,7 @@ import { Component, SimpleChanges } from '@angular/core';
   styleUrls: ['./rules.component.scss']
 })
 export class RulesComponent {
+
   public source_as_device = false;
   public destination_as_device = false;
 
@@ -26,12 +27,13 @@ export class RulesComponent {
   public new_source_port: number = 0;
   public new_destination: string = "";
   public new_destination_port: number = 0;
+  public has_been_copied:boolean = false;
 
   public f_option = false;
   // x implies f
   public x_option = false;
 
-  public isValid()  {
+  public isValid() {
     if (this.chain_selected == "")
       return false;
     if (this.action_selected == "")
@@ -43,30 +45,31 @@ export class RulesComponent {
 
   public outputRule() {
     let output;
-    if (this.chain_selected.includes('ROUTING'))  {
+    if (this.chain_selected.includes('ROUTING')) {
       output = `iptables -t nat -A ${this.chain_selected} `;
-    } else  {
+    } else {
       output = `iptables -A ${this.chain_selected} `;
     }
     if (this.source_as_device && this.source_device != '') {
-      output += `-i ${this.source_device}${this.source_port_selected == 0 || this.source_port_selected == null ? '' : ' --sport '+this.source_port_selected} `;
-    } else if (this.source_address != '')  {
-      output += `-s ${this.source_address}${this.source_mask == '' ? '' : '/' + this.source_mask}${this.source_port_selected == 0 || this.source_port_selected == null ? '' : ' --sport '+this.source_port_selected} `;
+      output += `-i ${this.source_device}${this.source_port_selected == 0 || this.source_port_selected == null ? '' : ' --sport ' + this.source_port_selected} `;
+    } else if (this.source_address != '') {
+      output += `-s ${this.source_address}${this.source_mask == '' ? '' : '/' + this.source_mask}${this.source_port_selected == 0 || this.source_port_selected == null ? '' : ' --sport ' + this.source_port_selected} `;
     }
     output += `-p ${this.traffic_selected} `;
     if (this.destination_as_device && this.destination_device != '') {
-      output += `-o ${this.destination_device}${this.destination_port_selected == 0 || this.destination_port_selected == null ? '' : ' --dport '+this.destination_port_selected}`;
-    } else if (this.destination_address != '')  {
-      output += `-d ${this.destination_address}${this.destination_mask == '' ? '' : '/' + this.destination_mask}${this.destination_port_selected == 0 || this.destination_port_selected == null ? '' : ' --dport '+this.destination_port_selected} `;
+      output += `-o ${this.destination_device}${this.destination_port_selected == 0 || this.destination_port_selected == null ? '' : ' --dport ' + this.destination_port_selected}`;
+    } else if (this.destination_address != '') {
+      output += `-d ${this.destination_address}${this.destination_mask == '' ? '' : '/' + this.destination_mask}${this.destination_port_selected == 0 || this.destination_port_selected == null ? '' : ' --dport ' + this.destination_port_selected} `;
     }
     output += `-j ${this.action_selected}`;
-    if (this.action_selected.includes('SNAT'))  {
+    if (this.action_selected.includes('SNAT')) {
       output += ` --to-source ${this.new_source}${this.new_source_port == 0 || this.new_source_port == null ? '' : ':' + this.new_source_port}`;
-    } else if (this.action_selected.includes('DNAT'))  {
+    } else if (this.action_selected.includes('DNAT')) {
       output += ` --to-destination ${this.new_destination}${this.new_destination_port == 0 || this.new_destination_port == null ? '' : ':' + this.new_destination_port}`;
     }
     return output;
   }
+
 
   public isSNAT() {
     return this.action_selected.includes('SNAT');
@@ -76,18 +79,36 @@ export class RulesComponent {
     return this.action_selected.includes('DNAT');
   }
 
-  public x_implies_f()  {
+  public x_implies_f() {
     if (!this.x_option)
       this.f_option = true;
   }
-  
+
   public f_if_x() {
-    if (!this.x_option)  {
+    if (!this.x_option) {
       this.f_option = true;
       return this.f_option;
     }
-    this.f_option = ! this.f_option;
+    this.f_option = !this.f_option;
     return this.f_option;
+  }
+
+  //if something changes in the form, reset the copy button
+
+
+
+  public copyToClipboard() {
+    const el = document.createElement('textarea');
+    this.has_been_copied = true;
+    el.value = this.outputRule();
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    document.getElementById('clipboard-text')?.classList.add('vanishing');
+    setTimeout(() => {
+      document.getElementById('clipboard-text')?.classList.remove('vanishing');
+    }, 6000);
   }
 
 }
